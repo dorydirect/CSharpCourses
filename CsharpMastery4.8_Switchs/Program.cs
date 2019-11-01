c6e209953c4f02c680721d73c9d8e39be9d49c0c
Dor Friedman <dorydirect@gmail.com>
2019-11-01 12:18:19 +0200

    CourseLessons

---
+++ CsharpMastery4.8_Switchs/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpMastery4._8_Switchs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Switch statments format in integers
+            int minAge = 21;
+            
+            switch (minAge)
+            {
+                case 5:
+                    Console.WriteLine("You are too young!");
+                    break;
+                case 21:
+                    Console.WriteLine("You are exactly in the right age! Come in");
+                    break;
+                default:
+                    Console.WriteLine("I am not sure what is your age!");
+                    break;
+
+
+            }
+            //Switch statments format in strings
+
+            string _name = "Dor";
+
+            switch (_name)
+            {
+                case "Dor":
+                        Console.WriteLine("Name is Dor");
+                        break;
+                default:
+                        Console.WriteLine("Name is not Dor");
+                        break;
+            }
+
+            Console.Read();
+        }
+    }
+}
