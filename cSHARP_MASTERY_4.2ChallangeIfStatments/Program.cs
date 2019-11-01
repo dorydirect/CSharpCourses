96f79a2f83d78fee6cdc45ee671fc69ac1847b3d
Dor Friedman <dorydirect@gmail.com>
2019-11-01 12:22:35 +0200

    ..

---
+++ cSHARP_MASTERY_4.2ChallangeIfStatments/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSHARP_MASTERY_4._2ChallangeIfStatments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _userInputTemperture = Console.ReadLine();
+            int temperture = int.Parse(_userInputTemperture);
+
+            if (temperture == 10)
+            {
+                Console.WriteLine("It's 10 degrees... a bit chilly");
+            }
+            else if (temperture < 10)
+            {
+                Console.WriteLine("It's less 10 than degrees... It's Very cold man!");
+            }
+            else 
+            {
+                Console.WriteLine("It's more than 10 degrees... Feels warm!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
