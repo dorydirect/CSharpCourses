8bdce78a06e1a4cf43714fb0ee0be2d6a12f6238
Dor Friedman <dorydirect@gmail.com>
2019-11-01 12:20:35 +0200

    ..

---
+++ CsharpMastery4.7ChallangeAgeVarifierIfStatments/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpMastery4._7ChallangeAgeVarifierIfStatments
+{
+    class Program
+    {
+        static private int minimumAge = 21;
+        static string _userAge;
+        static int userAgeAsInt => int.Parse(_userAge);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the club!");
+            Console.WriteLine("How old are you?");
+            _userAge = Console.ReadLine();
+            
+
+            if ( userAgeAsInt < minimumAge)
+            {
+                Console.WriteLine("You can't go in the club kiddo!");
+            }
+            else
+            {
+                Console.WriteLine("Come inside, first drink is free!");
+            }
+            Console.Read();
+
+        }
+    }
+}
---
+++ cSHARP_MASTERY_4.6ChallangeLoginSystem/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSHARP_MASTERY_4._6ChallangeLoginSystem
+{
+    class Program
+    {
+        //Varibles:
+        static string _username;
+        static string _password;
+
+        static void Main(string[] args)
+        {
+            Register();
+            Login();
+            Console.Read();
+        }
+        static void Register()
+        {
+            Console.WriteLine("Hello! Please Register to access your safe:");
+            Console.WriteLine("Please enter your username: ");
+            _username = Console.ReadLine();
+            Console.WriteLine("Thanks!");
+            Console.WriteLine("Please enter your password: ");
+            _password = Console.ReadLine();
+            Console.WriteLine("Registeration Completed");
+            Console.WriteLine("--------------------------------");
+        }
+        static void Login()
+        {
+            Console.WriteLine("Enter Username: ");
+
+            if (_username == Console.ReadLine())
+            {
+                Console.WriteLine("Enter Password: ");
+                if (_password == Console.ReadLine())
+                {
+                    Console.WriteLine("Login Succesful");
+                }
+                else
+                {
+                    Console.WriteLine("Password is incorrect, Please try again.");
+                    Login();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Username is incorrect, please try again. ");
+                Login();
+            }
+        }
+    }
+}
